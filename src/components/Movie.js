b24de5ffea17c59866ec313a4d0d3acd0ef32447
Martin Vuong Nguyen <ngumartin@gmail.com>
2020-06-11 18:15:13 -0700

    added info div, view detail

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,12 +4,15 @@ const Movie = (props) => {
     
     return (
         <div className='col s12 m6 l3'>
-            <div className='card-image waves-effect waves-block waves-light'>
-                {
-                    props.image == null ? <img src={`https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg`} alt='card image cap' style={{ width: '100%', height: 360}} /> : <img src={`http://image.tmdb.org/t/p/w185${props.image}`} alt='card image cap' style={{ width: '100%', height: 360}} />
-                }
+            <div className='card'>
+                <div className='card-image waves-effect waves-block waves-light'>
+                    {
+                        props.image == null ? <img src={`https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg`} alt='card image cap' style={{ width: '100%', height: 360}} /> : <img src={`http://image.tmdb.org/t/p/w185${props.image}`} alt='card image cap' style={{ width: '100%', height: 360}} />
+                    }
+                </div>
             </div>
-    
+                <div className='card-content'>
+                    <p> <a href='#'>View Default</a></p>                </div>
         </div>
     )
 }
